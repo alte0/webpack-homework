1d317b1de1e32eae63c0b28a75fa9e38d0af53a2
m.dmitriev <makc-dmv@yandex.ru>
2019-01-12 22:08:08 +0500

    setting project

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,64 @@
-module.exports = {};
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const env = process.env.NODE_ENV;
+const isDev = env !== "production";
+
+const webpackConfig = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      isDev: JSON.stringify(isDev)
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.[hash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new CleanWebpackPlugin(path.resolve(__dirname, "dist"), {})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["es2015", "react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: loader => [require("precss")()]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  devtool: isDev ? "eval-source-map" : "source-map"
+};
+
+module.exports = webpackConfig;
